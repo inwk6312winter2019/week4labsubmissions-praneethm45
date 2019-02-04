9f9aca09726697f2d04d59cfa9ac6db84c88abb5
Praneeth <pr642175@dal.ca>
2019-02-03 20:52:07 -0400

    lab4T1

---
+++ lab4T1.py
@@ -0,0 +1,25 @@
+import string
+def str_op():
+  myfile = open()
+  pun = string.punctuation
+  his = dict()
+  count = 0
+  listofwords = []
+  for cher in mylife:
+   char=char.strip()
+   for c in pun:
+    char = char.replace(c,"")
+    char = char.lower()
+    lis = char.split()
+    for word in lis:
+     listofwords.append(li)
+     for lists in listofwords:
+      if lists not in his:
+	his[lists] = 1
+      else:
+	his[lists] += 1
+	for key,value in his.items():
+	 if(value == 20):
+	  print(key)
+
+str_op()
